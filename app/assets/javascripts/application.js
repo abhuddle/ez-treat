82f1a0e74aefa8e87dbfc85ce80765e57015b638
abhuddle <andy.huddle@playonsports.com>
2015-05-08 09:17:35 -0400

    website v2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,6 @@
 //= require jquery
 //= require bootstrap-sprockets
+//= require agency
+//= require classie
+//= require cbpAnimatedHeader
+//= require twitter/bootstrap
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,5 @@
 class SiteController < ApplicationController
   def index
-
+    render :layout => false
   end
 end
